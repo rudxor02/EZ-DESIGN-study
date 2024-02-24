b9bded3881ca4cafc48e6675ce275ff4b4709398
rudxor02 <rudxor07@naver.com>
2024-02-24 20:11:48 +0900

    feat: week2

--- week2/main.py
+++ week2/main.py
@@ -1,13 +1,66 @@
-from typing import Union
+import time
+from typing import Any, Union
 
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
+class SlowSingletonSession:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print("Creating new instance")
+            cls._instance = super(SlowSingletonSession, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, "session"):
+            self.session = requests.Session()
+            self.wiki_url = "https://en.wikipedia.org/w/api.php"
+            self.cache: dict[str, Any] = {}
+
+    def _get_params(self, query: str):
+        return {
+            "action": "query",
+            "format": "json",
+            "list": "search",
+            "srsearch": query,
+        }
+
+    def get(self, query: str):
+        if query not in self.cache.keys():
+            params = self._get_params(query)
+            time.sleep(1)
+            response = self.session.get(url=self.wiki_url, params=params)
+            data = response.json()
+            self.cache[query] = data
+            return data
+        return self.cache[query]
+
+
+def dec(func):
+    def wrapper(request: Request):
+        result = func(request)
+        print(request.cookies)
+        print(request.headers)
+        # ip print
+        print(request.client.host)
+        return result
+
+    return wrapper
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @dec
+def read_root(request: Request):
+    query = request.query_params.get("query")
+    if query is None:
+        return {"error": "query is required"}
+    session = SlowSingletonSession()
+    return session.get(query)
 
 
 @app.get("/items/{item_id}")
